f9f55c53198419ea98251bd673a6d434b89bd5eb
John Notorgiacomo <fixed-term.John.Notorgiacomo@us.bosch.com>
2020-07-18 16:02:51 -0400

    starting to learn about javascript; moving to simple server next

---
+++ javascript_testing/printMessages.js
@@ -0,0 +1,9 @@
+const messageModule = require(’./messages’); 1
+
+messageModule.messages.forEach( (m) =&gt; { 2
+
+  console.log(m);
+
+});
+
+// NOTE: tutorial was unclear about how this works; first time with js and npm
---
+++ javascript_testing/zip_connection/README.md
@@ -0,0 +1,1 @@
+reference: https://www.sitepoint.com/build-a-simple-web-server-with-node-js/#:~:text=To%20get%20started%20with%20a,the%20project%20with%20npm%20init%20.
